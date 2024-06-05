bab34e49c939c5859d3de3241ddfca9d8330bdaf
Vatsal Patel <vatsalpatel@Vatsals-MacBook-Pro.local>
2024-06-05 17:47:01 -0400

    URL Updated

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js";
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import createTokenAndSaveCookie from "../jwt/generateToken.js"
 
 export const signup= async (req,res)=> {
